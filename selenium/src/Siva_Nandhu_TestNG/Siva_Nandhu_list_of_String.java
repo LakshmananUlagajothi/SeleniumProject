2c39231f77251f6b8182f2e352e6f4ce2aab5340
WELCOME <WELCOME@Desktop-LakshmananU>
2023-12-02 18:41:24 +0530

    added to selenium

---
+++ selenium/src/Siva_Nandhu_TestNG/Siva_Nandhu_list_of_String.java
@@ -0,0 +1,14 @@
+package Siva_Nandhu_TestNG;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Siva_Nandhu_list_of_String {
+	
+	@Test
+	public void testNandhu()
+	{
+		Reporter.log("Nandhu love siva", true);
+	}
+
+}
